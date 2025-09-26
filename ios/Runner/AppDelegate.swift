6ab7a061533083b2502db0af87be1d99b55cc801
hoseong1324 <hoseong1324@naver.com>
2025-09-26 16:13:26 +0900

    feat: 소셜 로그인 구현 완료 (카카오, 구글, 네이버)
    - flutter_naver_login 2.1.1 사용
    - flutter_naver_login 1.8.0으로 다운그레이드 후 API 수정
    - 카카오 SDK 초기화 및 앱/웹 폴백 로직 구현
    - 구글 로그인 구현
    - 네이버 로그인 구현 (null 체크로 성공 여부 확인)
    - iOS AppDelegate.swift에 플러그인 자동 등록 추가
    - 소셜 로그인 UI 구현 (로딩 인디케이터, 에러 처리)
    - AuthProfile 모델 및 SocialAuthService 인터페이스 구현
    - 네이티브 설정 완료 (Info.plist, AndroidManifest.xml)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,22 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate {
+
+  override func application(_ application: UIApplication,
+                            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    // 플러그인 자동 등록 (MissingPluginException 방지)
     GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // URL 콜백 처리
+  override func application(_ app: UIApplication,
+                            open url: URL,
+                            options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    return super.application(app, open: url, options: options)
   }
 }
